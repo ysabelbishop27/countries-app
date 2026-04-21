7152c8f4d64a74a49ae90034577594f258404644
ysabelbishop27 <ysabelbishop5150@gmail.com>
2026-04-21 15:45:32 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Countries App</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://backend-answer-keys.onrender.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  plugins: [react()],
+});
